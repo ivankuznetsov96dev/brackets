663ed195c4c146bad8617b28fd7a68bcff6c6129
Ivan Kuzniatsou <ivan.kuznetsov.dev@gmail.com>
2020-10-25 13:30:42 +0300

    feat: brackets task compleated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+// module.exports = function check(str, bracketsConfig) {
+  module.exports = function check(s) {
+    let pairs = {
+        '}':'{',
+        ']':'[',
+        ')':'(',
+    };
+    let stack = [];
+    for(let i = 0;i < s.length;++i){
+        switch(s.charAt(i)){
+            case '[': case '{':case '(':
+                stack.push(s.charAt(i));
+                break;
+            case ']': case '}':case ')':
+                if(isStackEmpty(stack) || peek(stack) !== pairs[s.charAt(i)]) return false;
+                stack.pop();
+                break;
+            case '"':
+                if(isStackEmpty(stack) || peek(stack) !== s.charAt(i)){
+                    stack.push(s.charAt(i));
+                }else{
+                    stack.pop();
+                }
+        }
+    }
+    return isStackEmpty(stack);
+}
+    function isStackEmpty(s){
+        return s.length === 0;
+}
+    function peek(s){
+        return s[s.length-1];
 }
